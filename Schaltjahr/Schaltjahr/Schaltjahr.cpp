cd66a7b2c09ba5fd695236eb70d0ce2155fd35e0
ofolta <ofolta@PC04111843>
2017-02-14 12:00:35 +0100

    Schaltjahr

---
+++ Schaltjahr/Schaltjahr/Schaltjahr.cpp
@@ -0,0 +1,21 @@
+// Schaljahr.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include <stdio.h>
+
+
+int main()
+{
+	int jahr;
+	int erg;
+	printf("Bitte geben Sie ein Jahr ein: \n");
+	scanf("%i", &jahr);
+	erg = jahr % 4 == 0 && (jahr % 100 != 0 || jahr % 400 == 0);
+	erg == 1 ? printf("%i ist ein Schaltjahr!", jahr) : printf("%i ist kein Schaltjahr!", jahr);
+
+
+
+
+
+	return 0;
+}
--- mixed_app/mixed_app/mixed_app.cpp
+++ mixed_app/mixed_app/mixed_app.cpp
@@ -4,6 +4,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"firstProjFun.h"
+#include"stdafx.h"
+
 
 
 
